d7d3008b8464fc9188fd76f1eeb8ba4ff550d4b3
chrischang <chrischang@nine-yi.com>
2018-09-08 20:38:57 +0800

    modify index

--- sw.js
+++ sw.js
@@ -1,11 +1,13 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
-
-if (workbox) {
-    console.log('Yay! Workbox is loaded 🎉');
-    workbox.routing.registerRoute(
-        new RegExp('.*\.json'),
-        workbox.strategies.networkFirst()
-    );
-} else {
-    console.log("Boo! Workbox didn't load 😬");
+if ( !workbox ) {
+    console.log("Workbox didn't load.");
+    return;
 }
+workbox.setConfig({ debug: true });
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+window.addEventListener('beforeinstallprompt', function(event) {
+    console.log("before install prompt");
+    event.prompt();
+})
