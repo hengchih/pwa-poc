44804a6665d8416f5e4cff93d4a42c2e298cfb92
chrischang <chrischang@nine-yi.com>
2018-09-08 21:00:40 +0800

    update

--- sw.js
+++ sw.js
@@ -1,10 +1,5 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
 
-window.addEventListener('beforeinstallprompt', function(event) {
-    console.log("before install prompt");
-    event.prompt();
-});
-
 if ( workbox ) {
     workbox.setConfig({ debug: true });
     workbox.skipWaiting();
