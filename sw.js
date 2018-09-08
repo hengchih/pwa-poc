c64cd122705d1406553aca0e20eb663e020df1df
chrischang <chrischang@nine-yi.com>
2018-09-08 18:44:52 +0800

    modify index

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
 
 if (workbox) {
-    console.log(`Yay! Workbox is loaded 🎉`);
+    console.log('Yay! Workbox is loaded 🎉');
     workbox.routing.registerRoute(
         new RegExp('.*\.json'),
         workbox.strategies.networkFirst()
     );
 } else {
-    console.log(`Boo! Workbox didn't load 😬`);
+    console.log("Boo! Workbox didn't load 😬");
 }
