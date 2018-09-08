8258b0a429e876841180290cc5ab71f64d50b3b9
chrischang <chrischang@nine-yi.com>
2018-09-08 20:59:34 +0800

    update

--- sw.js
+++ sw.js
@@ -1,13 +1,16 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js');
-if ( !workbox ) {
-    console.log("Workbox didn't load.");
-    return;
-}
-workbox.setConfig({ debug: true });
-workbox.skipWaiting();
-workbox.clientsClaim();
 
 window.addEventListener('beforeinstallprompt', function(event) {
     console.log("before install prompt");
     event.prompt();
 });
+
+if ( workbox ) {
+    workbox.setConfig({ debug: true });
+    workbox.skipWaiting();
+    workbox.clientsClaim();
+
+}else{
+    console.log("Workbox didn't load.");
+}
+
